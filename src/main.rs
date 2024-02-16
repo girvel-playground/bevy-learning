8ebe50d8e8e0f36c248daa5fb1cf355470bc502c
girvel <widauka@ya.ru>
2024-02-17 03:41:24 +0500

    Hardcoded movement forward on W

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use bevy::log::prelude::*;
 use bevy::{
     prelude::*,
     sprite::Anchor,
@@ -7,12 +8,12 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, stupid_falling)
+        .add_systems(Update, keyboard_input_system)
         .run();
 }
 
 #[derive(Component)]
-struct StupidFalling;
+struct Controlled;
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font = asset_server.load("fonts/FiraSans-Bold.ttf");
@@ -38,15 +39,19 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         transform: Transform::from_translation(Vec3::ZERO),
         text_anchor: Anchor::Center,
         ..default()
-    }, StupidFalling));
+    }, Controlled));
 }
 
-fn stupid_falling(
+const MOVEMENT_SPEED: f32 = 150.;
+
+fn keyboard_input_system(
+    input: Res<Input<KeyCode>>,
     time: Res<Time>,
-    mut query: Query<&mut Transform, With<StupidFalling>>,
+    mut query: Query<&mut Transform, With<Controlled>>,
 ) {
     for mut transform in &mut query {
-        transform.translation.y -= 50. * time.delta_seconds();
+        if input.pressed(KeyCode::W) {
+            transform.translation += MOVEMENT_SPEED * time.delta_seconds() * Vec3::Y;
+        }
     }
 }
-
